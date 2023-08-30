3606f429890e2e24a56abd0eaaba45ad39d53bdc
Narendra Patwardhan <narendra@machinelearning.one>
2023-08-30 11:15:26 +0200

    Made error handling more uniform with errors.Join, errors.New and fmt.Sprintf

--- cli/cmd/info.go
+++ cli/cmd/info.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
 	"machinelearning.one/flux/info"
 )
 
@@ -12,7 +13,7 @@ var infoCmd = &cobra.Command{
 	Use:   "info",
 	Short: "Prints extended information about usage",
 	Args:  cobra.NoArgs,
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, _ []string) {
 		// Print the embedded README.md file
 		fmt.Println(string(info.Readme))
 	},
